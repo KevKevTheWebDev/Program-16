38689f88b33699c9a6a0e9f5d09d1d3f6824c183
KevKevTheWebDev <student.122454@worc.k12.ma.us>
2015-07-21 16:57:41 -0400

    Update and rename README.md to apples.java

---
+++ apples.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class apples{
+	public static void main(String args[]){
+	tuna tunaObject = new tuna("Kelsey");
+	tuna tunaObject2 = new tuna("Nicole");
+	tunaObject.saying();
+	tunaObject2.saying();
+	}
+}
